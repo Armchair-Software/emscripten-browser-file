202e6fa5906c1a21587aa185be3be7226c34fbc8
SlowRiot <slowriot@voxelstorm.com>
2023-03-06 08:43:53 +0000

    ensure Module members survive closure name mangling, and don't copy callback data and pointer, just pass them directly

--- emscripten_browser_file.h
+++ emscripten_browser_file.h
@@ -29,17 +29,16 @@ EM_JS_INLINE(void, upload, (char const *accept_types, upload_handler callback, v
   /// Accept-types are in the format ".png,.jpeg,.jpg" as per https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/accept
   /// Upload handler callback signature is:
   ///   void my_handler(std::string const &filename, std::string const &mime_type, std::string_view buffer, void *callback_data = nullptr);
-  globalThis.open_file_callback_data = callback_data;
-  globalThis.open_file = function(e) {
+  globalThis["open_file"] = function(e) {
     const file_reader = new FileReader();
     file_reader.onload = (event) => {
       const uint8Arr = new Uint8Array(event.target.result);
       const num_bytes = uint8Arr.length * uint8Arr.BYTES_PER_ELEMENT;
-      const data_ptr = Module._malloc(num_bytes);
-      const data_on_heap = new Uint8Array(Module.HEAPU8.buffer, data_ptr, num_bytes);
+      const data_ptr = Module["_malloc"](num_bytes);
+      const data_on_heap = new Uint8Array(Module["HEAPU8"].buffer, data_ptr, num_bytes);
       data_on_heap.set(uint8Arr);
-      Module.ccall('upload_file_return', 'number', ['string', 'string', 'number', 'number', 'number', 'number'], [event.target.filename, event.target.mime_type, data_on_heap.byteOffset, uint8Arr.length, callback, open_file_callback_data]);
-      Module._free(data_ptr);
+      Module["ccall"]('upload_file_return', 'number', ['string', 'string', 'number', 'number', 'number', 'number'], [event.target.filename, event.target.mime_type, data_on_heap.byteOffset, uint8Arr.length, callback, callback_data]);
+      Module["_free"](data_ptr);
     };
     file_reader.filename = e.target.files[0].name;
     file_reader.mime_type = e.target.files[0].type;
@@ -48,7 +47,7 @@ EM_JS_INLINE(void, upload, (char const *accept_types, upload_handler callback, v
 
   var file_selector = document.createElement('input');
   file_selector.setAttribute('type', 'file');
-  file_selector.setAttribute('onchange', 'open_file(event)');
+  file_selector.setAttribute('onchange', 'globalThis["open_file"](event)');
   file_selector.setAttribute('accept', UTF8ToString(accept_types));
   file_selector.click();
 });
@@ -65,7 +64,7 @@ EM_JS_INLINE(void, download, (char const *filename, char const *mime_type, void 
   /// Offer a buffer in memory as a file to download, specifying download filename and mime type
   var a = document.createElement('a');
   a.download = UTF8ToString(filename);
-  a.href = URL.createObjectURL(new Blob([new Uint8Array(Module.HEAPU8.buffer, buffer, buffer_size)], {type: UTF8ToString(mime_type)}));
+  a.href = URL.createObjectURL(new Blob([new Uint8Array(Module["HEAPU8"].buffer, buffer, buffer_size)], {type: UTF8ToString(mime_type)}));
   a.click();
 });
 #pragma GCC diagnostic pop
